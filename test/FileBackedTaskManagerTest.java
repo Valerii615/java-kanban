047129d612437233ff89fe5146cd994f35a7c8f7
Valerii <Kovaleff615@yandex.ru>
2024-05-06 16:59:02 +0300

    добавление тестов проверки на несуществующий файл и обработку неверных типов данных

--- test/FileBackedTaskManagerTest.java
+++ test/FileBackedTaskManagerTest.java
@@ -1,11 +1,13 @@
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import ru.valerii.task_tracker.model.Epic;
 import ru.valerii.task_tracker.model.Subtask;
 import ru.valerii.task_tracker.model.Task;
 import ru.valerii.task_tracker.service.FileBackedTaskManager;
 import ru.valerii.task_tracker.service.Status;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -47,6 +49,7 @@ public class FileBackedTaskManagerTest {
      */
     @Test
     void restoringTasksFromFile() {
+        fileBackedTaskManager.removeAllTaskNotSave();
         fileBackedTaskManager.loadFromFile("src/ru/valerii/task_tracker/task_storage.csv");
         assertFalse(fileBackedTaskManager.getTasks().isEmpty(), "Задачи типа Task не восстановились из файла");
         assertFalse(fileBackedTaskManager.getEpics().isEmpty(), "Задачи типа Epic не восстановились из файла");
@@ -60,7 +63,38 @@ public class FileBackedTaskManagerTest {
     void correctAssignmentOfAnIdAfterRecoveryFromFile() {
         fileBackedTaskManager.loadFromFile("src/ru/valerii/task_tracker/task_storage.csv");
         fileBackedTaskManager.addTask(new Task("Обычная задача №1", "Описание", Status.NEW));
-        System.out.println(fileBackedTaskManager.getIdCount());
+        System.out.println();
+        assertEquals(fileBackedTaskManager.getIdCount(), fileBackedTaskManager.getTaskOfId(fileBackedTaskManager.getIdCount()).getId(), "Неверно присвоенный id");
+    }
+
+
+    /**
+     * обработка неккоректных форматов данных
+      */
+    @Test
+    public void processingIncorrectDataFormats() {
+        Wrapper wrapper = new Wrapper("src/test_file/task_storage_id.csv");
+        Exception exception = assertThrows(RuntimeException.class, wrapper);
+        assertEquals("Неверный формат id", exception.getMessage(), "получено неверное исключение");
+        Wrapper wrapper1 = new Wrapper("src/test_file/task_storage_type_task.csv");
+        Exception exception1 = assertThrows(RuntimeException.class, wrapper1);
+        assertEquals("Неверный тип задачи", exception1.getMessage(), "получено неверное исключение");
+        Wrapper wrapper2 = new Wrapper("src/test_file/task_storage_status.csv");
+        Exception exception2 = assertThrows(RuntimeException.class, wrapper2);
+        assertEquals("Неверный формат статуса", exception2.getMessage(), "получено неверное исключение");
+        Wrapper wrapper3 = new Wrapper("src/test_file/task_storage_epic_is_null.csv");
+        Exception exception3 = assertThrows(RuntimeException.class, wrapper3);
+        assertEquals("Epic с заданным id не существует", exception3.getMessage(), "получено неверное исключение");
+    }
+
+    /**
+     * тест на чтение несуществующего файла
+     */
+    @Test
+    void readingNonExistentFile() {
+        Wrapper wrapper = new Wrapper("storage.csv");
+        Exception exception = assertThrows(RuntimeException.class, wrapper);
+        assertEquals("Ошибка Чтения файла", exception.getMessage(), "получено неверное исключение");
     }
 
     /**
@@ -77,5 +111,18 @@ public class FileBackedTaskManagerTest {
         assertTrue(fileBackedTaskManager.getTasks().isEmpty(), "Задачи типа Task не удалились из постоянной памяти");
         assertTrue(fileBackedTaskManager.getEpics().isEmpty(), "Задачи типа Epic из постоянной памяти");
         assertTrue(fileBackedTaskManager.getSubtasks().isEmpty(), "Задачи типа Subtask из постоянной памяти");
+    }
+
+}
+class Wrapper implements Executable {
+    String fileName;
+
+    public Wrapper(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void execute() {
+        FileBackedTaskManagerTest.fileBackedTaskManager.loadFromFile(fileName);
     }
 }
