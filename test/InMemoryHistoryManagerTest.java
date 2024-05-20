8e08976379adef408536f148ec8a80310cc55009
Valerii <Kovaleff615@yandex.ru>
2024-05-20 12:27:02 +0300

    добавление тестов на дублирование, удаления из истории: начало, середина, конец, получение пустой истории

--- test/InMemoryHistoryManagerTest.java
+++ test/InMemoryHistoryManagerTest.java
@@ -1,3 +1,6 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.valerii.task_tracker.model.Task;
 import ru.valerii.task_tracker.service.Managers;
@@ -6,18 +9,58 @@ import ru.valerii.task_tracker.service.TaskManager;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class InMemoryHistoryManagerTest {
-    TaskManager taskManager = Managers.getDefault();
+    static TaskManager taskManager;
+
+    @BeforeEach
+    void addTask() {
+        taskManager = Managers.getDefault();
+        taskManager.addTask(new Task("1", "1"));
+        taskManager.addTask(new Task("2", "2"));
+        taskManager.addTask(new Task("3", "3"));
+        taskManager.addTask(new Task("4", "4"));
+        taskManager.addTask(new Task("5", "5"));
+    }
 
     /**
-     * проверка на добавление задачи в историю и получение истории задач
+     * проверка на добавление задачи в историю и дублирование
      */
     @Test
     void addTaskInHistory() {
-        taskManager.addTask(new Task("1", "1"));
-        taskManager.addTask(new Task("2", "2"));
         taskManager.getTaskOfId(1);
         assertEquals(1, taskManager.getHistory().size(), "неверная длинна истории");
         taskManager.getTaskOfId(2);
         assertEquals(2, taskManager.getHistory().size(), "неверная длинна истории");
+        taskManager.getEpicOfId(1);
+        assertEquals(2, taskManager.getHistory().size(), "неверная длинна истории");
+        taskManager.removeAllTask();
     }
+
+    /**
+     * Удаление из истории: начало, середина, конец.
+     */
+    @Test
+    void deletionFromHistoryBeginningMiddleEnd() {
+        taskManager.getTaskOfId(1);
+        taskManager.getTaskOfId(2);
+        taskManager.getTaskOfId(3);
+        taskManager.getTaskOfId(4);
+        taskManager.getTaskOfId(5);
+        taskManager.removeTaskOfId(1);
+        assertEquals(4, taskManager.getHistory().size(), "неверная длинна истории (удаление задачи id-1)");
+        taskManager.removeTaskOfId(3);
+        assertEquals(3, taskManager.getHistory().size(), "неверная длинна истории (удаление задачи id-3)");
+        taskManager.removeTaskOfId(5);
+        assertEquals(2, taskManager.getHistory().size(), "неверная длинна истории (удаление задачи id-5");
+        taskManager.removeAllTask();
+    }
+
+    /**
+     * получение пустой истории задач
+     */
+    @AfterAll
+    static void gettingAnEmptyTaskHistory() {
+        assertEquals(0, taskManager.getHistory().size(), "неверная длинна истории");
+    }
+
+
 }
