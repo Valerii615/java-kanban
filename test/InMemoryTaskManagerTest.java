2a69e6f4a83c7b57ef319b20861c1a9ee598e6e6
Valerii <Kovaleff615@yandex.ru>
2024-05-20 12:28:26 +0300

    добавление тестов на корректный расчет пересечения времени задач, адоптация тестов к новой функциональности приложения

--- test/InMemoryTaskManagerTest.java
+++ test/InMemoryTaskManagerTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import ru.valerii.task_tracker.model.Epic;
 import ru.valerii.task_tracker.model.Subtask;
 import ru.valerii.task_tracker.model.Task;
@@ -8,6 +9,8 @@ import ru.valerii.task_tracker.service.Managers;
 import ru.valerii.task_tracker.service.Status;
 import ru.valerii.task_tracker.service.TaskManager;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,15 +28,24 @@ class InMemoryTaskManagerTest {
     static void addingAllTypesOfTasks() {
         taskManager = Managers.getDefault();
 
-        taskManager.addTask(new Task("Обычная задача №1", "Описание", Status.NEW));
-        taskManager.addTask(new Task("Обычная задача №2", "Описание", Status.NEW));
+        taskManager.addTask(new Task("Обычная задача №1", "Описание", Status.NEW, LocalDateTime.of(2024,5,17,12,0), Duration.ofMinutes(30)));
+        taskManager.addTask(new Task("Обычная задача №2", "Описание", Status.NEW, LocalDateTime.of(2024,5,17,13,0), Duration.ofMinutes(40)));
 
         taskManager.addEpic(new Epic("Эпик задача №1 с 2-мя подзадачами", "Описание"));
         taskManager.addEpic(new Epic("Эпик задача №2 с одной подзадачей", "Описание"));
 
-        taskManager.addSubtask(new Subtask("Подзадача №1.Эпик1", "Описание", Status.NEW, 3));
-        taskManager.addSubtask(new Subtask("Подзадача №2.Эпик1", "Описание", Status.NEW, 3));
-        taskManager.addSubtask(new Subtask("Подзадача №1.Эпик2", "Описание", Status.NEW, 4));
+        taskManager.addSubtask(new Subtask("Подзадача №1.Эпик1", "Описание", Status.NEW, 3, LocalDateTime.of(2024,5,17,14,0), Duration.ofMinutes(30)));
+        taskManager.addSubtask(new Subtask("Подзадача №2.Эпик1", "Описание", Status.NEW, 3, LocalDateTime.of(2024,5,17,15,0), Duration.ofMinutes(30)));
+        taskManager.addSubtask(new Subtask("Подзадача №1.Эпик2", "Описание", Status.NEW, 4, LocalDateTime.of(2024,5,17,16,0), Duration.ofMinutes(30)));
+    }
+    /**
+     * проверка на корректный расчет пересечения времени задач
+     */
+    @Test
+    public void correctnessOfTheCalculationOfTheTimeIntersectionOfTasks() {
+        Wrapper1 wrapper1 = new Wrapper1();
+        Exception exception = assertThrows(RuntimeException.class, wrapper1);
+        assertEquals("Задача не может быть добавлена так как пересекается с задачей 'id = 1'время начала задачи = 2024-05-17T12:00'время завершения задачи = 2024-05-17T12:30", exception.getMessage(), "получено неверное исключение");
     }
 
     /**
@@ -144,7 +156,7 @@ class InMemoryTaskManagerTest {
     @Test
     void deletingFromTheHistory() {
         taskManager.removeTaskOfId(1);
-        assertEquals(2, taskManager.getHistory().size(), "задача не удалена из истории");
+        assertEquals(5, taskManager.getHistory().size(), "задача не удалена из истории");
         taskManager.removeSubtaskOfId(5);
         assertEquals(1, taskManager.getAllSubtaskOfEpic(3).size(), "осталcь неактуальная подзадача");
     }
@@ -171,4 +183,13 @@ class InMemoryTaskManagerTest {
         System.out.println(taskManager.getEpics());
         System.out.println(taskManager.getSubtasks());
     }
+}
+class Wrapper1 implements Executable {
+
+    @Override
+    public void execute(){
+        InMemoryTaskManagerTest.taskManager.addTask(new Task("Обычная задача №1", "Описание", Status.NEW, LocalDateTime.of(2024,5,17,12,0), Duration.ofMinutes(30)));
+    }
+
+
 }
