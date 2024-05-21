f9efbaaa93891044e1cb34b6fc73c07b5ac79fbf
Valerii <Kovaleff615@yandex.ru>
2024-05-21 16:58:27 +0300

    гид-хаб завис на проверке, пытаюсь выяснить в чем дело добавлением кода в main

--- src/ru/valerii/task_tracker/Main.java
+++ src/ru/valerii/task_tracker/Main.java
@@ -1,8 +1,17 @@
 package ru.valerii.task_tracker;
 
+import ru.valerii.task_tracker.model.Task;
+import ru.valerii.task_tracker.service.FileBackedTaskManager;
+import ru.valerii.task_tracker.service.Status;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        FileBackedTaskManager fileBackedTaskManager = new FileBackedTaskManager();
+        fileBackedTaskManager.addTask(new Task("Обычная задача №1", "Описание", Status.NEW, LocalDateTime.of(2024,5,17,12,0), Duration.ofMinutes(30)));
+        System.out.println(fileBackedTaskManager.getTasks());
     }
 }
