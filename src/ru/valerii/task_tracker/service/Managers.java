e928179f9e7d0ba48c7644dd983042f2f69174d2
Valerii <Kovaleff615@yandex.ru>
2024-05-29 16:41:16 +0300

    добавление метода получения Gson

--- src/ru/valerii/task_tracker/service/Managers.java
+++ src/ru/valerii/task_tracker/service/Managers.java
@@ -1,5 +1,11 @@
 package ru.valerii.task_tracker.service;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class Managers {
     public static TaskManager getDefault() {
         return new InMemoryTaskManager();
@@ -7,5 +13,12 @@ public class Managers {
 
     public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
+    }
+
+    public static Gson getGson() {
+        return new GsonBuilder()
+                .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeTypeAdapter())
+                .registerTypeAdapter(Duration.class, new DurationTypeAdapter())
+                .create();
     }
 }
