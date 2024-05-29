e5b0dd7ddccc704f9ae0cfbc7365958b17d6aa5c
Valerii <Kovaleff615@yandex.ru>
2024-05-29 16:55:36 +0300

    удаление неиспользуемых импортов

--- src/ru/valerii/task_tracker/Main.java
+++ src/ru/valerii/task_tracker/Main.java
@@ -1,18 +1,5 @@
 package ru.valerii.task_tracker;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import ru.valerii.task_tracker.model.Epic;
-import ru.valerii.task_tracker.model.Subtask;
-import ru.valerii.task_tracker.model.Task;
-import ru.valerii.task_tracker.service.FileBackedTaskManager;
-import ru.valerii.task_tracker.service.LocalDateTimeTypeAdapter;
-import ru.valerii.task_tracker.service.Managers;
-import ru.valerii.task_tracker.service.Status;
-
-import java.time.Duration;
-import java.time.LocalDateTime;
-
 public class Main {
 
     public static void main(String[] args) {
