8ff022c5e1e22760367d9a88824dad85971b6431
Valerii <Kovaleff615@yandex.ru>
2024-05-30 12:39:40 +0300

    добавил метод создания контекста из листа

--- src/ru/valerii/task_tracker/server/HttpTaskServer.java
+++ src/ru/valerii/task_tracker/server/HttpTaskServer.java
@@ -5,20 +5,19 @@ import ru.valerii.task_tracker.service.FileBackedTaskManager;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HttpTaskServer {
     public static final int PORT = 8080;
     FileBackedTaskManager fileBackedTaskManager;
     HttpServer server;
+    List<String> paths = new ArrayList<>(List.of("/tasks", "/subtasks", "/epics", "/history", "/prioritized"));
 
     public HttpTaskServer(FileBackedTaskManager fileBackedTaskManager) throws IOException {
         this.fileBackedTaskManager = fileBackedTaskManager;
         server = HttpServer.create(new InetSocketAddress(PORT), 0);
-        server.createContext("/tasks", new TaskHandler(fileBackedTaskManager));
-        server.createContext("/subtasks", new TaskHandler(fileBackedTaskManager));
-        server.createContext("/epics", new TaskHandler(fileBackedTaskManager));
-        server.createContext("/history", new TaskHandler(fileBackedTaskManager));
-        server.createContext("/prioritized", new TaskHandler(fileBackedTaskManager));
+        createContext();
         server.start();
         System.out.println("HTTP-сервер запущен на " + PORT + " порту!");
     }
@@ -30,6 +29,11 @@ public class HttpTaskServer {
 
     public static void main(String[] args) throws IOException {
         HttpTaskServer httpTaskServer = new HttpTaskServer(new FileBackedTaskManager());
+    }
 
+    public void createContext() {
+        for (String path : paths) {
+            server.createContext(path, new TaskHandler(fileBackedTaskManager));
+        }
     }
 }
